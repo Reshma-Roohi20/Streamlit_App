792d6efcc106931a9f31a6e380dd96141505d461
Reshma-Roohi20 <124127928+Reshma-Roohi20@users.noreply.github.com>
2023-02-03 16:58:45 +0530

    Updated  App.py

--- App.py
+++ App.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+ 
+st.title("Calculator App using Streamlit")
+ 
+# creates a horizontal line
+st.write("Calculator")
+
+# input 1
+num1 = st.number_input(label="Enter first number")
+ 
+# input 2
+num2 = st.number_input(label="Enter second number")
+
+st.write("Operation")
+ 
+operation = st.radio("Select an operation to perform:",
+                    ("Add", "Subtract", "Multiply", "Divide"))
